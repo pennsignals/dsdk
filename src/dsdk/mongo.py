d858a157a4fce6d0d8f08cc9d8d8f554c3898c2d
Michael Becker <michael.becker@uphs.upenn.edu>
2019-11-21 14:42:28 -0500

    store_evidence: add exclude_cols
    `exclude_cols` allow us to not store all DataFrame columns in mongo

--- src/dsdk/mongo.py
+++ src/dsdk/mongo.py
@@ -1,3 +1,6 @@
+from functools import partial
+from functools import wraps
+
 from pandas import DataFrame
 
 from dsdk.utils import create_new_batch
@@ -16,15 +19,24 @@ def needs_batch_id(func):
 
 
 # TODO: optional parameter to specify fields that aren't retained
-def store_evidence(func):
+def store_evidence(func=None, *, exclude_cols=None):
+    if exclude_cols is None:
+        exclude_cols = []
+    exclude_cols = frozenset(exclude_cols)
+    if func is None:
+        return partial(store_evidence, exclude_cols=exclude_cols)
+
+    @wraps(func)
     @needs_batch_id
     def wrapper(self, *args, **kwargs):
         evidence = func(self, *args, **kwargs)
         if isinstance(evidence, DataFrame):
             # TODO: We need to check column types and convert as needed
             evidence["batch_id"] = self.batch.batch_id
-            res = self.batch.mongo[self.name].insert_many(evidence.to_dict(orient="records"))
-            assert evidence.shape[0] == len(res.inserted_ids)
+            evidence_keep = evidence[[c for c in evidence.columns if c not in exclude_cols]]
+            res = self.batch.mongo[self.name].insert_many(evidence_keep.to_dict(orient="records"))
+            assert evidence_keep.shape[0] == len(res.inserted_ids)  # TODO: Better exception
+            evidence.drop(columns=["batch_id"], inplace=True)
         else:
             raise NotImplementedError(
                 "Serialization is not implemented for type {}".format(type(evidence))
