d329fc6696c4268fc649e29f0eea89f59b29ad47
Jason Lubken <jlubken@noreply.github.com>
2020-09-11 13:40:40 -0400

    Simplify epoch_ms

---
+++ test/test_time_conversions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Test epoch_ms and utc datetime conversions."""
+
+from dsdk.service import (
+    epoch_ms_from_utc_datetime,
+    now_utc_datetime,
+    utc_datetime_from_epoch_ms,
+)
+
+
+def test_conversions():
+    """Test conversions."""
+    expected = now_utc_datetime()
+    epoch_ms = epoch_ms_from_utc_datetime(expected)
+    actual = utc_datetime_from_epoch_ms(epoch_ms)
+    assert expected == actual
