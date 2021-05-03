ecfe1514c73ac4401ed198a7f63128286c1c5f06
Jason Lubken <jlubken@users.noreply.github.com>
2021-05-03 13:01:13 -0400

    Move sql statement to debug log

--- src/dsdk/persistor.py
+++ src/dsdk/persistor.py
@@ -104,7 +104,8 @@ class AbstractPersistor:
         for table in self.tables:
             try:
                 statement = self.extant(table)
-                logger.info(self.EXTANT, statement)
+                logger.info(self.EXTANT, table)
+                logger.debug(self.EXTANT, statement)
                 cur.execute(statement)
                 for row in cur:
                     n, *_ = row
