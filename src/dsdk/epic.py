c2da5ef896577e9183ba6ccf33adb64d65ded256
Jason Lubken <jlubken@users.noreply.github.com>
2021-08-18 15:32:14 -0400

    Remove soap

--- src/dsdk/epic.py
+++ src/dsdk/epic.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """Epic."""
 
+from base64 import b64encode
 from contextlib import contextmanager
+from datetime import datetime
 from logging import getLogger
 from select import select
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Tuple
+from urllib.parse import quote
 
 from cfgenvy import yaml_type
 
@@ -12,20 +15,16 @@ from .postgres import Persistor as Postgres
 
 try:
     from requests import Session
-    from zeep import Client
-    from zeep.transports import Transport
 except ImportError:
-    Client = None
     Session = None
-    Transport = None
 
 logger = getLogger(__name__)
 
 
-class Interconnect:  # pylint: disable=too-many-instance-attributes
-    """Interconnect."""
+class Epic:  # pylint: disable=too-many-instance-attributes
+    """Epic."""
 
-    YAML = "!interconnect"
+    YAML = "!epic"
 
     @classmethod
     def as_yaml_type(cls, tag: Optional[str] = None):
@@ -49,90 +48,74 @@ class Interconnect:  # pylint: disable=too-many-instance-attributes
 
     def __init__(
         self,
-        *,
-        authorization: str,
+        client_id: str,  # 00000000-0000-0000-0000-000000000000
         cookie: str,
-        postgres: Postgres,
-        service_name: str,
-        urn: str,
-        wsdl: str,
+        password: str,
+        url: str,
+        flowsheet_id: str,
         comment: str = "Not for clinical use.",
-        connect_timeout: int = 5,
-        contact_id_type: str = "csn",
-        flowsheet_id: str = "3040015333",
+        contact_id_type: str = "CSN",
+        flowsheet_id_type: str = "internal",
         flowsheet_template_id: str = "3040005300",
         flowsheet_template_id_type: str = "internal",
-        lookback_hours: int = 72,
-        operation_timeout: int = 3,
-        patient_id_type: str = "uid",
-        user_id: str = "pennsignals",
+        patient_id_type: str = "UID",
+        username: str = "Pennsignals",
         user_id_type: str = "external",
-    ) -> None:
+        user_id: str = "PENNSIGNALS",
+    ):
         """__init__."""
-        self.authorization = authorization
-        self.cookie = cookie
-        self.postgres = postgres
-        self.wsdl = wsdl
+        self.authorization = b"Basic " + \
+            b64encode(f"EMP${username}:{password}".encode("utf-8"))
+        self.client_id = client_id
         self.comment = comment
         self.contact_id_type = contact_id_type
-        self.connect_timeout = connect_timeout
+        self.cookie = cookie
         self.flowsheet_id = flowsheet_id
+        self.flowsheet_id_type = flowsheet_id_type
         self.flowsheet_template_id = flowsheet_template_id
         self.flowsheet_template_id_type = flowsheet_template_id_type
-        self.lookback_hours = lookback_hours
-        self.operation_timeout = operation_timeout
         self.patient_id_type = patient_id_type
+        self.url = url
         self.user_id = user_id
         self.user_id_type = user_id_type
-        self.urn = urn
+
+    def __call__(self):
+        """__call__."""
+        with self.session() as session:
+            with self.listener() as listener:
+                with self.postgres.commit() as cur:
+                    self.recover(cur, session)
+                for each in listener:
+                    self.on_notify(each, cur, session)
 
     def as_yaml(self) -> Dict[str, Any]:
         """As yaml."""
         return {
             "authorization": self.authorization,
+            "client_id": self.client_id,
             "comment": self.comment,
             "contact_id_type": self.contact_id_type,
             "cookie": self.cookie,
             "flowsheet_id": self.flowsheet_id,
+            "flowsheet_id_type": self.flowsheet_id_type,
             "flowsheet_template_id": self.flowsheet_template_id,
             "flowsheet_template_id_type": self.flowsheet_template_id_type,
-            "lookback_hours": self.lookback_hours,
             "patient_id_type": self.patient_id_type,
-            "postgres": self.postgres,
-            "urn": self.urn,
+            "url": self.url,
             "user_id": self.user_id,
             "user_id_type": self.user_id_type,
-            "wsdl": self.wsdl,
         }
 
     @contextmanager
-    def interconnect(self):
-        """Interconnect soap client."""
-        session = Session()
-        session.headers.update(
-            {
-                "authorization": self.authorization,
-                "cookie": self.cookie,
-            }
-        )
-        session.verify = False
-        transport = Transport(
-            session=session,
-            timeout=self.connect_timeout,
-            opration_timeout=self.operation_timeout,
-        )
-        client = Client(
-            wsdl=self.wsdl,
-            service_name=self.service_name,
-            transport=transport,
-        )
-        yield client
+    def listener(self):
+        """Listener."""
+        raise NotImplementedError()
 
-    def listen(self, listen, cur, interconnect):
+    def listen(self, listen, cur, session):
         """Listen."""
         while True:
             readers, _, exceptions = select(
-                [listen], [], [listen], self.epic.poll_timeout
+                [listen], [], [listen], self.poll_timeout
             )
             if exceptions:
                 break
@@ -142,7 +125,7 @@ class Interconnect:  # pylint: disable=too-many-instance-attributes
                 while listen.notifies:
                     yield listen.notified.pop()
 
-    def on_notify(self, event, cur, intervconnect):
+    def on_notify(self, event, cur, session):
         """On postgres notify handler."""
         logger.debug(
             "NOTIFY: %(id)s.%(channel)s.%(payload)s",
@@ -153,148 +136,189 @@ class Interconnect:  # pylint: disable=too-many-instance-attributes
             },
         )
 
-    def soap(self, event, cur, interconnect):
-        """Soap call."""
+    def on_success(self, entity, cur, message):
+        """On success."""
         raise NotImplementedError()
 
+    def on_error(self, entity, cur, message):
+        """On error."""
+        raise NotImplementedError()
 
-class Notifier(Interconnect):
+    @contextmanager
+    def session(self):
+        """Session."""
+        session = Session()
+        session.verify = False
+        session.headers.update({
+            "Authorization": self.authorization,
+            "Cookie": self.cookie,
+            "Epic-Client-ID": self.client_id,
+            "Epic-User-ID": self.user_id,
+            "Epic-User-IDType": self.user_id_type,
+        })
+        yield session
+
+
+class Notifier(Epic):
     """Notifier."""
 
-    YAML = "!interconnectnotifier"
+    YAML = "!epicnotifier"
 
-    def __call__(self):
-        """__call__."""
-        postgres = self.epic.postgres
+    @classmethod
+    def as_yaml_type(cls, tag: Optional[str] = None):
+        """As yaml type."""
+        yaml_type(
+            cls,
+            tag or cls.YAML,
+            init=cls._yaml_init,
+            repr=cls._yaml_repr,
+        )
+
+    @classmethod
+    def _yaml_init(cls, loader, node):
+        """Yaml init."""
+        return cls(**loader.construct_mapping(node, deep=True))
+
+    @classmethod
+    def _yaml_repr(cls, dumper, self, *, tag):
+        """Yaml repr."""
+        return dumper.represent_mapper(tag, self.as_yaml())
+
+    @contextmanager
+    def listener(self):
+        """Listener."""
+        postgres = self.postgres
         sql = postgres.sql
-        with self.interconnect() as interconnect:
-            with postgres.listen(sql.prediction.listen) as listener:
-                with postgres.commit() as cur:
-                    self.recover(cur, interconnect)
-                for each in listener:
-                    self.on_notify(each, cur, interconnect)
+        with postgres.listen(sql.prediction.listen) as listener:
+            yield listener
 
-    def on_notify(self, event, cur, interconnect):
+    def on_notify(self, event, cur, session):
         """On notify."""
-        super.on_notify(event, cur, interconnect)
+        super.on_notify(event, cur, session)
         sql = self.postgres.sql
         cur.execute(sql.prediction.recent, event.id)
         for each in cur.fetchall():
-            self.soap(each, cur, interconnect)
+            ok, message = self.rest(each, session)
+            if ok:
+                self.on_success(each, cur, message)
+            else:
+                self.on_error(each, cur, message)
 
-    def recover(self, cur, interconnect):
+    def recover(self, cur, session):
         """Recover."""
         sql = self.epic.postgres.sql
         cur.execute(sql.epic.notification.recover)
         for each in cur.fetchall():
-            self.soap(each, cur, interconnect)
+            ok, message = self.rest(each, session)
+            if ok:
+                self.on_success(each, cur, message)
+            else:
+                self.on_error(each, cur, message)
 
-    def soap(self, prediction, cur, interconnect):
-        """Soap."""
-        sql = self.postgres.sql
-        print(dir(interconnect))
-        response = interconnect.service.AddFlowsheetValue(
-            Comment=self.comment,
-            ContactID=prediction["csn"],
-            ContactIDType=self.contact_id_type,
-            FlowsheetID=self.flowsheet_id,
-            FlowsheetTemplateID=self.flowsheet_template_id,
-            FlowsheetTemplateTypeID=self.flowsheet_template_type_id,
-            FlowsheetTypeID=self.flowsheet_type_id,
-            InstantValueTaken=self.instant_value_taken,
-            PatientID=prediction["empi"],
-            PatientIDType=self.patient_id_type,
-            UserID=self.user_id,
-            UserIDType=self.user_id_type,
-            Value=prediction["score"],
+    def rest(self, prediction, session) -> Tuple[bool, Any]:
+        """Rest."""
+        query = {
+            "comment": self.comment,
+            "contact_id_type": self.contact_id_type,
+            "flowsheet_id": self.flowsheet_id,
+            "flowsheet_id_type": self.flowsheet_id_type,
+            "flowsheet_template_id": self.flowsheet_template_id,
+            "flowsheet_template_id_type": self.flowsheet_template_id_type,
+            "patient_id_type": self.patient_id_type,
+            "user_id": self.user_id,
+            "user_id_type": self.user_id_type,
+        }
+        query.update({
+            "contact_id": prediction["csn"],
+            "instant_value_taken": prediction["create_on"].strftime(
+                "%Y-%m-%dT%H:%M:%SZ"),
+            "patient_id": prediction["empi"],
+            "value": prediction["score"],
+        })
+
+        url = self.url.format(**{
+            key: quote(value.encode('utf-8'))
+            for key, value in query.items()})
+        logger.info(url)
+
+        response = session.post(
+            url=url,
+            data={},
         )
 
-        print(response)
-
-        ok = response.Success
-        if ok:
-            cur.execute(
-                sql.epic.notification.insert,
-                {"prediction_id": prediction["id"]},
-            )
-        else:
-            cur.execute(
-                sql.epic.notification_error.insert,
-                {
-                    "description": response.text,
-                    "name": response.reason,
-                    "prediction_id": prediction["id"],
-                },
-            )
+        return response.status_code == 200, response
 
 
-class Verifier(Interconnect):
+class Verifier(Epic):
     """Verifier."""
 
-    YAML = "!interconnectverifier"
+    YAML = "!epicverifier"
 
-    def __call__(self):
-        """__call__."""
-        postgres = self.epic.postgres
+    @contextmanager
+    def listener(self):
+        """Listener."""
+        postgres = self.postgres
         sql = postgres.sql
-        with self.interconnect() as interconnect:
-            with postgres.listen(sql.notification.listen) as listen:
-                with postgres.commit() as cur:
-                    self.recover(cur, interconnect)
-                self.listen(listen, cur, interconnect)
+        with postgres.listen(sql.notification.listen) as listener:
+            yield listener
 
-    def on_notify(self, event, cur, interconnect):
+    def on_notify(self, event, cur, session):
         """On notify."""
-        super.on_notify(event, cur, interconnect)
+        super.on_notify(event, cur, session)
         sql = self.postgres.sql
         cur.execute(sql.notification.recent, event.id)
         for each in cur.fetchall():
-            self.soap(each, cur, interconnect)
+            ok, response = self.rest(each, session)
+            if ok:
+                self.on_success(each, session, response)
+            else:
+                self.on_error(each, session, response)
 
-    def recover(self, cur, interconnect):
-        """Recover."""
-        sql = self.epic.postgres.sql
-        cur.execute(sql.epic.verification.recover)
-        for each in cur.fetchall():
-            self.soap(each, cur, interconnect)
-
-    def soap(self, notification, cur, interconnect):
-        """Soap."""
-        postgres = self.postgres
-        sql = postgres.sql
-        response = interconnect.GetFlowsheetRows(
-            ContactID=notification["csn"],
-            ContactTypeID=self.contact_type_id,
-            FlowsheetRowIDs=[notification["flowsheet_row_id"]],
-            LookBackHours=self.look_back_hours,
-            PatientID=notification["empi"],
-            PatienTypeID=self.patient_type_id,
-            UserID=self.user_id,
-            UserTypeID=self.user_type_id,
+    def on_success(self, notification, cur, response):
+        """On sucess."""
+        cur.execute(
+            self.postgres.sql.epic.verification.insert,
+            {"prediction_id": notification["id"]},
         )
 
-        print(response)
+    def on_error(self, notification, cur, response):
+        """On error."""
+        cur.execute(
+            self.postgres.sql.epic.verification_error.insert,
+            {
+                "description": response.text,
+                "name": response.reason,
+                "prediction_id": notification["id"],
+            },
+        )
 
-        ok = response.Success
+    def rest(self, notification, session) -> Tuple[bool, Any]:
+        """Rest."""
+        data = {
+            "ContactIDType": self.contact_id_type,
+            "LookbackHours": self.lookback_hours,
+            "PatientIDType": self.patient_id_type,
+            "UserID": self.user_id,
+            "UserIDType": self.user_id_type,
 
-        if ok:
-            cur.execute(
-                sql.epic.verification.insert,
-                {"notification_id": notification["id"]},
-            )
-        else:
-            cur.execute(
-                sql.epic.verification_error.insert,
-                {
-                    "description": response.text,
-                    "name": response.reason,
-                    "notification_id": notification["id"],
-                },
-            )
+        data.update({
+            "ContactID": notification["csn"],
+            "FlowsheetRowIDs": [{
+                "ID": self.flowsheet_id,
+                "IDType": self.flowsheet_id_type,
+            }],
+            "PatientID": notification["empi"],
+        })
+        response = session.post(
+            url=self.url,
+            data=data,
+        )
+
+        return response.status_code == 200, response
 
 
-def main(empi, csn):
-    """Main."""
+def test_notifier(csn, empi, score):
+    """Rest."""
     from os import getcwd
     from os.path import join as pathjoin
 
@@ -303,32 +327,36 @@ def main(empi, csn):
     from .asset import Asset
 
     Asset.as_yaml_type()
-    Notifier.as_yaml_type()
     Postgres.as_yaml_type()
+    Notifier.as_yaml_type()
     parser = Parser()
     cwd = getcwd()
+
     notifier = parser.parse(
         argv=(
             "-c",
-            pathjoin(cwd, "local", "test.wsdl.yaml"),
+            pathjoin(cwd, "local", "test.notifier.yaml"),
             "-e",
-            pathjoin(cwd, "secrets", "test.wsdl.env"),
+            pathjoin(cwd, "secrets", "test.notifier.env"),
         )
     )
 
-    notification = {
+    prediction = {
+        "create_on": datetime.utcnow(),
         "csn": csn,
         "empi": empi,
-        "score": 0,
+        "score": score,
     }
 
-    with notifier.interconnect() as interconnect:
-        with notifier.postgres.commit() as cur:
-            notifier.soap(notification, cur, interconnect)
+    with notifier.session() as session:
+        ok, response = notifier.rest(prediction, session)
+        print(ok)
+        print(response)
 
 
 if __name__ == "__main__":
-    main(
+    test_notifier(
         csn="278820881",
-        empi="833065951",
+        empi="8330651951",
+        score="0.5",
     )
